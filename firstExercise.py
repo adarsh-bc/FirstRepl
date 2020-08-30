0b4d534484d163b4af0b16bbfaa5c1eb3b395e6f
adarshbc <adarsh.bc@gmail.com>
2020-08-30 14:11:39 +0000

    password exercise

---
+++ firstExercise.py
@@ -0,0 +1,4 @@
+user_name = input("Username")
+password = input("password")
+
+print('hi {user_name}, your password {password} is 6 letters long')
--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print ("Hello World")
+user_name = input("Username :")
+password = input("password :")
+
+#length=replace(password,"*")
+
+
+
+print('hi {0}, your password {1} is {2} letters long'.format(user_name,password.replace(password,"*" * len(password)),
+len(password)))
